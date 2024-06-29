00a7e40c4c02f84ad3fd7737d6a1efbc418923b8
Vovchick4 <vovaspodpivas@gmail.com>
2024-06-29 18:25:31 +0300

    feat assets url mix

--- config/app.php
+++ config/app.php
@@ -59,6 +59,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'mix_url' => env('MIX_ASSET_URL', null),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
